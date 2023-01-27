a4a0769d74e5f705869b0d8f2baa63faaf717262
PavelGodes <pgodes1@gmail.com>
2023-01-27 11:31:00 +0200

    HW #36(starting definition)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Input } from './components/Input';
 import { Timer } from './components/Timer';
 import {random} from '../components/utils/random'
 
+//import
+
 function App() {
 //   
 return <div></div>
