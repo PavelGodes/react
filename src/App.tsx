291296c12729607dd05b153dd1d393a4744ba903
PavelGodes <pgodes1@gmail.com>
2023-01-28 15:19:17 +0200

    hw#36 (with functions)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Input } from './components/Input';
-import { Timer } from './components/Timer';
-import {random} from '../components/utils/random'
-
-//import
-
+import React from "react";
+import { getRandomNumber } from "./utils/random";
+import { getRandomMatrix } from "./utils/random";
+import { getRandomArrayElement } from "./utils/random";
+import { getRandomDate } from "./utils/random";
 function App() {
-//   
-return <div></div>
+  console.log(getRandomNumber(5, 10));
+  console.log(getRandomNumber(5, 10, false, true));
+  console.log(getRandomNumber(10, 5));
+
+  const matrix = getRandomMatrix(3, 4, 5, 10);
+  console.log(matrix);
+
+  const colors = ["red", "green", "blue", "purple"];
+  console.log(getRandomArrayElement(colors));
+
+  const date = getRandomDate(2000, 2023);
+  console.log(date);
+  return <div></div>;
 }
 export default App;
